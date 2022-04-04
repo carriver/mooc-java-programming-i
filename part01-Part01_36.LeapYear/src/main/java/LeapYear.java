0d5410a7c6f1aa388eb73d3e9e71304e5121b48a
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-04 19:13:06 -0400

    completed exercise

--- part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -6,20 +6,11 @@ public class LeapYear {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Type a year: ");
-        float year = Float.parseFloat(scan.nextLine());
-        
-        if (year % 4 == 0) {
-            if(year % 100 != 0 ) {
-                System.out.println("The year is a leap year.");
-            } else {
-                if (year % 400 != 0){
-                    System.out.println("The year is not a leap year.");
-                } else {
-                    System.out.println("The year is a leap year.");
-                }
-            }
-        } else {   
+        System.out.print("Give a year: ");
+        int input = Integer.valueOf(scan.nextLine());
+        if (input % 4 == 0 && (input % 100 != 0 || input % 400 == 0)) {
+            System.out.println("The year is a leap year.");
+        } else {
             System.out.println("The year is not a leap year.");
         }
 
