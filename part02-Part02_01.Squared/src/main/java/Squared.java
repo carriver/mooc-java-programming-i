7ab7165f7083b29c3bf32fe5490328d9182fa640
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-05 14:58:59 -0400

    completed exercise

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -5,6 +5,10 @@ public class Squared {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int input = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println(input * input);
 
     }
 }
