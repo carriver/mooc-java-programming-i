00ca0bdfa04bd344370517c7c8f79fc7e20ea483
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-05 20:32:52 -0400

    completed exercise

--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,28 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int numbers = 0;
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+
+            if (input == 0) {
+                break;
+            }
+
+            if (input != 0 && input > 0) {
+                numbers = numbers + 1;
+                sum = sum + input;
+
+            }
+        }
+
+        if (numbers == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println("Average of the numbers: " + (double) sum / numbers);
+        }
     }
 }
