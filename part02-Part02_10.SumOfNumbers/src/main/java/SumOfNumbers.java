6a2da7d9aaceb6dddf5c43c2ea961433342071ea
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-05 20:21:28 -0400

    completed exercise

--- part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -5,6 +5,23 @@ public class SumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int numbers = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+
+            if (input == 0) {
+                break;
+            }
+
+            if (input != 0) {
+                numbers = numbers + input;
+            }
+        }
+
+        System.out.println("Sum of the numbers: " + numbers);
 
     }
 }
