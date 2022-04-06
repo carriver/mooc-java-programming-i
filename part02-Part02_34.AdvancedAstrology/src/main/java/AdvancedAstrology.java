c76ea5c0016d42b4530352dbb23af3f657a0a0a3
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-06 09:28:16 -0400

    completed exercise

--- part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -3,18 +3,53 @@ public class AdvancedAstrology {
 
     public static void printStars(int number) {
         // part 1 of the exercise
+        int i = 1;
+        while (i <= number) {
+            System.out.print("*");
+            i++;
+        }
+        System.out.println("");
     }
 
     public static void printSpaces(int number) {
         // part 1 of the exercise
+        int i = 1;
+        while (i <= number) {
+            System.out.print(" ");
+            i++;
+        }
     }
 
     public static void printTriangle(int size) {
         // part 2 of the exercise
+        int i = 1;
+        int j = size - 1;
+        while (i <= size) {
+            printSpaces(j);
+            printStars(i);
+            i++;
+            j--;
+        }
+
     }
 
     public static void christmasTree(int height) {
         // part 3 of the exercise
+        int i = 1;
+        int j = height - 1;
+        int k = 0;
+
+        while (i <= height) {
+            printSpaces(j);
+            printStars(i + k);
+            i++;
+            j--;
+            k++;
+        }
+        printSpaces(height-2);
+        printStars(3);
+        printSpaces(height-2);
+        printStars(3);
     }
 
     public static void main(String[] args) {
