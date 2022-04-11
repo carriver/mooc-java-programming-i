2cc4b37ef0353a90233102b16bb5237378452c78
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-10 23:57:34 -0400

    completed exercise

--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -6,6 +6,22 @@ public class AgeOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int age = 0;
+
+        while (true) {
+            String text = scanner.nextLine();
+
+            if (text.equals("")) {
+                break;
+            }
+
+            String[] pieces = text.split(",");
+
+            if (age < Integer.valueOf(pieces[1])) {
+                age = Integer.valueOf(pieces[1]);
+            }
+        }
+        System.out.println("Age of the oldest: " + age);
 
     }
 }
