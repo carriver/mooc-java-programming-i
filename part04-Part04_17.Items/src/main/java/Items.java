1c676faf899b8230c8c33e921f5816d9eb7559f7
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-11 18:42:11 -0400

    completed exercise

--- part04-Part04_17.Items/src/main/java/Items.java
+++ part04-Part04_17.Items/src/main/java/Items.java
@@ -10,5 +10,23 @@ public class Items {
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+
+            // Add to the list a new item
+            // whose name is the previous user input
+            items.add(new Item(name));
+        }
+        
+        System.out.println();
+        
+        for (Item item : items) {
+            System.out.println(item);
+        }
+
     }
 }
