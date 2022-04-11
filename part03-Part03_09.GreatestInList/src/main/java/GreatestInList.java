b997f7706e3b9fd9f31fddd76083760cf7d76f28
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-10 21:16:55 -0400

    completed exercise

--- part03-Part03_09.GreatestInList/src/main/java/GreatestInList.java
+++ part03-Part03_09.GreatestInList/src/main/java/GreatestInList.java
@@ -11,12 +11,21 @@ public class GreatestInList {
         while (true) {
             int input = Integer.valueOf(scanner.nextLine());
             if (input == -1) {
+                int largest = list.get(0);
+
+                for (int i = 0; i < list.size(); i++) {
+                    int number = list.get(i);
+                    if (largest < number) {
+                        largest = number;
+                    }
+                }
+                System.out.println("The greatest number: " + largest);
                 break;
             }
 
             list.add(input);
         }
-        
+
         System.out.println("");
 
         // implement finding the greatest number in the list here
