a408266962710b6c73b2b8d99c568058165b9404
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-10 23:44:50 -0400

    completed exercise

--- part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -6,6 +6,17 @@ public class FirstWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String text = scanner.nextLine();
+
+            if (text.equals("")) {
+                break;
+            }
+
+            String[] pieces = text.split(" ");
+
+            System.out.println(pieces[0]);
+        }
 
     }
 }
