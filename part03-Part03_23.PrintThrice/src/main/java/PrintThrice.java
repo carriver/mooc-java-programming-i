804bf9d29d59f59c404399df30ef82b5c0ee69be
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-10 23:15:47 -0400

    completed exercise

--- part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
+++ part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
@@ -6,7 +6,10 @@ public class PrintThrice {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        String input = scanner.nextLine();
+        System.out.print(input);
+        System.out.print(input);
+        System.out.print(input);
 
     }
 }
