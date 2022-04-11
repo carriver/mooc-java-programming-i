728e4062641282b086c3c8c5bd555cf50032d67b
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-11 00:01:49 -0400

    completed exercise

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -6,6 +6,23 @@ public class NameOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int age = 0;
+        String name = "name";
 
+        while (true) {
+            String text = scanner.nextLine();
+
+            if (text.equals("")) {
+                break;
+            }
+
+            String[] pieces = text.split(",");
+
+            if (age < Integer.valueOf(pieces[1])) {
+                age = Integer.valueOf(pieces[1]);
+                name = pieces[0];
+            }
+        }
+        System.out.println("Name of the oldest: " + name);
     }
 }
