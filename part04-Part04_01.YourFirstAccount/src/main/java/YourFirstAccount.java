475ff542712baec51fd59f340de48596fb6e7c97
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-11 02:54:42 -0400

    completed exercise

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,11 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+
+        Account account = new Account("My account", 100.00);
+
+        account.deposit(20);
+        System.out.println(account);
+
     }
 }
