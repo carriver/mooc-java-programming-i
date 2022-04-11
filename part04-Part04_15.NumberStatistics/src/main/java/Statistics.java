9e8e2fc3683722d9c8db2e534591bad24485e3c6
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-11 05:18:53 -0400

    completed exercise

--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,35 @@
 
 public class Statistics {
+
+    private int count;
+    private int sum;
+
+    public Statistics() {
+        int count = 0;
+        int sum = 0;
+
+    }
+
+    public void addNumber(int number) {
+        // adds a new number to the statistics
+        count += 1;
+        sum += number;
+
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int sum() {
+        return sum;
+    }
+
+    public double average() {
+        if (count != 0) {
+        return 1.0 * sum/count;
+        } else {
+            return 0;
+        }
+    }
 }
