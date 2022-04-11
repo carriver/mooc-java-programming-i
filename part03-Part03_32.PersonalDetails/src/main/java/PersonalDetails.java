60f35c2d5a985ca54fdb13fe3f83c53d4d2b61a8
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-11 00:15:27 -0400

    completed exercise

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -7,6 +7,31 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int length = 0;
+        int sum = 0;
+        String name = "name";
+        int count = 0;
+
+        while (true) {
+            String text = scanner.nextLine();
+
+            if (text.equals("")) {
+                break;
+            }
+
+            String[] pieces = text.split(",");
+
+            sum += Integer.valueOf(pieces[1]);
+
+            count += 1;
+            
+            if (length < pieces[0].length()) {
+                length = pieces[0].length();
+                name = pieces[0];
+            }
+        }
+        System.out.println("Longest name: " + name);
+        System.out.println("Average of the birth years: " + 1.0 * sum/count);
 
     }
 }
