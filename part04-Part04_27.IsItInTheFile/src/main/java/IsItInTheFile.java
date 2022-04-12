84026e44e12712eb095cc5cd6e41e067f693fea7
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-12 00:29:18 -0400

    completed exercise

--- part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -1,5 +1,6 @@
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class IsItInTheFile {
@@ -12,6 +13,26 @@ public class IsItInTheFile {
 
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
+        
+        ArrayList<String> list = new ArrayList<>();
+
+        try (Scanner reader = new Scanner(Paths.get(file))) {
+
+            while (reader.hasNextLine()) {
+                String row = reader.nextLine();
+
+                list.add(row);
+            }
+        }
+            catch (Exception e) {
+            System.out.println("Reading the file " + file + " failed.");
+        }
+        
+        if (list.contains(searchedFor)) {
+            System.out.println("Found!");
+        } else {
+            System.out.println("Not found.");
+        }
 
     }
 }
