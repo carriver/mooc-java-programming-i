fceff67c987c4e95700741b54b9de51de56762ea
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-19 11:43:04 -0400

    completed exercise

--- part05-Part05_14.Books/src/main/java/Book.java
+++ part05-Part05_14.Books/src/main/java/Book.java
@@ -17,4 +17,25 @@ public class Book {
         return publicationYear;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        // if the variables are located in the same position, they are equal
+        if (this == obj) {
+            return true;
+        }
+
+        // if the compared object is not of type Bird, the objects are not equal
+        if (!(obj instanceof Book)) {
+            return false;
+        }
+
+        // convert the object to a Bird object
+        Book comparedBook = (Book) obj;
+
+        // if the values of the object variables are equal, the objects are, too
+        return this.name.equals(comparedBook.name)
+                && this.publicationYear == comparedBook.publicationYear;
+
+    }
+
 }
