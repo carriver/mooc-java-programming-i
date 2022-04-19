1b103f64b1ba4e9f1173660fa01c10bcee86dcab
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-19 12:47:48 -0400

    completed exercise

--- part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
+++ part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
@@ -11,6 +11,44 @@ public class SimpleDate {
         this.year = year;
     }
 
+    public void advance() {
+        if (this.day < 30) {
+            this.day++;
+        } else if (this.month < 12) {
+            this.day = 1;
+            this.month++;
+        } else {
+            this.day = 1;
+            this.month = 1;
+            this.year++;
+        }
+    }
+
+    public void advance(int howManyDays) {
+        for (int i = 0; i < howManyDays; i++) {
+            this.advance();
+        }
+    }
+
+    public SimpleDate afterNumberOfDays(int days) {
+        SimpleDate clone = this.clone();
+
+        clone.advance(days);
+
+        SimpleDate newDate = new SimpleDate(clone.day, clone.month, clone.year);
+
+        return newDate;
+    }
+
+    @Override
+    protected SimpleDate clone() {
+        // create a new counter object that receives the value of the cloned counter as its initial value
+        SimpleDate clone = new SimpleDate(this.day, this.month, this.year);
+
+        // return the clone to the caller
+        return clone;
+    }
+
     @Override
     public String toString() {
         return this.day + "." + this.month + "." + this.year;
@@ -25,8 +63,8 @@ public class SimpleDate {
             return true;
         }
 
-        if (this.year == compared.year && this.month == compared.month &&
-                 this.day < compared.day) {
+        if (this.year == compared.year && this.month == compared.month
+                && this.day < compared.day) {
             return true;
         }
 
