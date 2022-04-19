7b875f5582d27c87079fcd0f1913c2c57399c35a
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-19 08:46:00 -0400

    completed exercise

--- part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
+++ part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
@@ -3,5 +3,13 @@ public class MainProgram {
 
     public static void main(String[] args) {
         // Test your counter here
+
+        Counter counter = new Counter(1);
+        counter.increase(2);
+        counter.increase();
+        counter.decrease();
+        counter.decrease(2);
+
+        System.out.println(counter.value());
     }
 }
