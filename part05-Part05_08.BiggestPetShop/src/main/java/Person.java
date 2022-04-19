89b4e8aa3612a9176040f10e56eed4cd09062b2f
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-19 10:31:48 -0400

    completed exercise

--- part05-Part05_08.BiggestPetShop/src/main/java/Person.java
+++ part05-Part05_08.BiggestPetShop/src/main/java/Person.java
@@ -19,7 +19,11 @@ public class Person {
 
     @Override
     public String toString() {
-        return this.nimi;
+        return this.name + ", has a friend called " + pet.getName() + " (" + pet.getBreed() + ")";
+    }
+
+    public String getName() {
+        return name;
     }
 
 }
