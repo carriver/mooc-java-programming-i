2849063d5e2253dfee84d887ac4a6640fcd00388
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-20 05:24:12 -0400

    completed exercise

--- part06-Part06_03.MessagingService/src/main/java/Main.java
+++ part06-Part06_03.MessagingService/src/main/java/Main.java
@@ -4,5 +4,12 @@ public class Main {
     public static void main(String[] args) {
 
         // Try out your class here
+        Message message = new Message("Tom", "Hello");
+        System.out.println(message);
+        
+        MessagingService messages = new MessagingService();
+        
+        messages.add(message);
+        System.out.println(messages.getMessages());;
     }
 }
