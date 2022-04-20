5ac680e878fd68430a3d1705c31bd6e40ba12613
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-20 04:51:28 -0400

    completed exercise

--- part06-Part06_02.Stack/src/main/java/Main.java
+++ part06-Part06_02.Stack/src/main/java/Main.java
@@ -4,6 +4,27 @@ public class Main {
     public static void main(String[] args) {
 
         // Try out your class here
+        Stack s = new Stack();
+        System.out.println(s.isEmpty());
+        System.out.println(s.values());
+        s.add("Value");
+        System.out.println(s.isEmpty());
+        System.out.println(s.values());
+        String taken = s.take();
+        System.out.println(s.isEmpty());
+        System.out.println(s.values());
+        System.out.println(taken);
+
+        Stack s2 = new Stack();
+        s2.add("1");
+        s2.add("2");
+        s2.add("3");
+        s2.add("4");
+        s2.add("5");
+
+        while (!s2.isEmpty()) {
+            System.out.println(s2.take());
+        }
 
     }
 }
---
+++ part06-Part06_02.Stack/src/main/java/Stack.java
@@ -0,0 +1,29 @@
+
+import java.util.ArrayList;
+
+public class Stack {
+
+    private ArrayList<String> stack;
+
+    public Stack() {
+        this.stack = new ArrayList<>();
+    }
+
+    public boolean isEmpty() {
+        return this.stack.isEmpty();
+    }
+
+    public void add(String value) {
+        this.stack.add(0, value);
+    }
+
+    public ArrayList<String> values() {
+        return this.stack;
+    }
+    
+    public String take() {
+        String topE = this.stack.get(0);
+        this.stack.remove(0);
+        return topE;
+    }
+}
