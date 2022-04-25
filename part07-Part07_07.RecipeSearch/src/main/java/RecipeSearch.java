b2801e371a3db32f7ea337531f6b28edefd438a5
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-25 09:48:31 -0400

    completed exercise

--- part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
+++ part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
@@ -1,12 +1,28 @@
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RecipeSearch {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("File to read: ");
+        String file = reader.nextLine();
+        System.out.println("\n");
+
+        try (Scanner scanner = new Scanner(Paths.get(file))) {
+            RecipeLibrary library = new RecipeLibrary(scanner);
+            library.readFile();
+
+            UserInterface ui = new UserInterface(reader, library);
+            ui.start();
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
     }
 
